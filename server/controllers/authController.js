1fa40293d59ce1f2ad10f6d3f316ba629c191c53
wendelle <superwendell46@gmail.com>
2020-11-03 16:05:57 -0500

    configure sign up route using User model

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,21 @@
-module.exports.signup_post = (req, res, next) => {
-    res.send("signup")
+const createError = require('http-errors')
+const { registerSchema } = require('../helpers/validation.schema')
+const { User } = require('../models/User')
+
+module.exports.signup_post = async(req, res, next) => {
+    try {
+        const results = await registerSchema.validateAsync(req.body)
+        // check if user exists
+        const userExists = await User.findOne({ where: { email: results.email } })
+        if(userExists) throw createError.Conflict(`${results.email} is already registered`)
+
+        const newUser = await User.create(results)
+        res.status(201).send({newUser})
+    } catch (error) {
+        // unprocessable request
+        if(error.isJoi === true) error.status = 422;
+        next(error)
+    }
 }
 
 module.exports.login_post = (req, res, next) => {
---
+++ server/helpers/validation.schema.js
@@ -0,0 +1,9 @@
+const Joi = require('@hapi/joi');
+
+const registerSchema = Joi.object({
+    username: Joi.string().min(2).lowercase().regex(/^[a-z\d\-_\s]+$/i).required(),
+    email: Joi.string().email().lowercase().required(),
+    password: Joi.string().min(5).required(),
+})
+
+module.exports = { registerSchema }
