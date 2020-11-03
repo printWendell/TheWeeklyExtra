de48aa620e5b3f1a7a5e20b4db0cc646d1427eba
wendelle <superwendell46@gmail.com>
2020-11-03 14:33:34 -0500

    add error handler with http-errors

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const cors = require('cors');
+const createError = require('http-errors');
 const Port = process.env.PORT || 5000;
 
 const { User } = require('./models/User');
@@ -27,6 +28,23 @@ app.get('/api/', async (req, res) => {
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+// catch all error route
+app.use(async (req, res, next) => {
+  //   pass to error handler
+  next(createError.NotFound());
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.send({
+    error: {
+      status: err.status || 500,
+      message: err.message,
+    },
+  });
 });
 
 
