c5656cdb694650722a74d5579a14d6b89b348892
wendelle <superwendell46@gmail.com>
2021-01-03 14:01:15 -0500

    add getSavedArticle function; style profile component

--- client/src/Components/Profile/Profile.js
+++ client/src/Components/Profile/Profile.js
@@ -1,18 +1,117 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Redirect } from 'react-router-dom';
 import Cookie from 'js-cookie';
-import { cookieStrToObj } from '../../Helpers/functions';
+import { cookieStrToObj, getSavedArticles } from '../../Helpers/functions';
+import Articles from '../Articles/Articles';
+import { Avatar, Box, Divider } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  profileUser: {
+    background: 'linear-gradient(to bottom, #8D91A4, #8D91A4 50%, white 50%)',
+    border: 'solid 1px #bdbdbd',
+    borderRadius: '5px',
+    padding: '3rem',
+  },
+  profileUserIcon: {
+    marginRight: '3rem',
+  },
+  userIcon: {
+    backgroundColor: '#2E3B55',
+    width: theme.spacing(15),
+    height: theme.spacing(15),
+    fontSize: theme.spacing(8),
+  },
+  [theme.breakpoints.down('xs')]: {
+    profileUser: {
+      background: 'linear-gradient(to bottom, #8D91A4, #8D91A4 60%, white 40%)',
+      display: 'block',
+    },
+    profileUserIcon: {
+      marginRight: '0',
+    },
+    userIcon: {
+      width: theme.spacing(11),
+      height: theme.spacing(11),
+      fontSize: theme.spacing(6),
+      marginLeft: 'auto',
+      marginRight: 'auto',
+    },
+    profileUserInfo: {
+      textAlign: 'center',
+      '& h1': {
+        fontSize: theme.spacing(3),
+      },
+      '& p': {
+        marginTop: '1.5rem',
+        fontSize: theme.spacing(1.7),
+      },
+    },
+    profileArticlesHeader: {
+      textAlign: 'center',
+    },
+  },
+}));
 
 function Profile() {
+  const classes = useStyles();
   const cookie = Cookie.get('user');
   let user = cookieStrToObj(cookie);
-  // console.log(user.email);
+
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    getSavedArticles().then((data) => setArticles(data));
+  }, []);
 
   if (!user.email) return <Redirect to="/login" />;
+
   return (
-    <div>
-      <h1>Profile: {user.name}</h1>
-    </div>
+    <Box className="profile" mt={3}>
+      <Box
+        className={`profile-user, ${classes.profileUser}`}
+        display="flex"
+        flexDirection="row"
+        mb={5}
+      >
+        <Box className={`profile-user--icon, ${classes.profileUserIcon}`}>
+          <Avatar className={`user--icon, ${classes.userIcon}`}>
+            {user.name.charAt(0)}
+          </Avatar>
+        </Box>
+        <Box className={`profile-user--info, ${classes.profileUserInfo}`}>
+          <h1>{user.name}</h1>
+          <p>{user.email}</p>
+          <p>{articles.length} Saved Articles</p>
+        </Box>
+      </Box>
+      <main className="profile-articles">
+        <Box
+          mb={5}
+          className={`profile-articles--header, ${classes.profileArticlesHeader}`}
+        >
+          <h1>Your Articles</h1>
+          <Divider />
+        </Box>
+        {articles.map((article, index) => (
+          <article key={index} className="profile-articles--article">
+            {/* saved article key names doesnt match article keys from news api */}
+            <Articles
+              article={{
+                author: article.author,
+                title: article.title,
+                description: article.description,
+                publishedAt: article.published_at,
+                source: article.source,
+                url: article.link_url,
+                urlToImage: article.img_url,
+              }}
+              articleSection="bottom"
+            />
+          </article>
+        ))}
+      </main>
+    </Box>
   );
 }
 
--- client/src/Helpers/functions.js
+++ client/src/Helpers/functions.js
@@ -7,34 +7,57 @@ export function cookieStrToObj(str) {
   return obj;
 }
 
+// check if article is saved
+export const check = async (title) => {
+  const res = await fetch('/api/articles/');
+  const data = await res.json();
+  return data.some(function (article) {
+    return article.title === title;
+  });
+};
+
+// get user saved articles api call
+export const getSavedArticles = async () => {
+  const res = await fetch('/api/articles/');
+  return await res.json();
+};
+
 // save article api call
-export const saveUserArticle = (email, article, savedArticleTime) => {
-  return fetch('/api/articles/save', {
-    method: 'POST',
-    body: JSON.stringify({
-      email,
-      author: article.author,
-      title: article.title,
-      description: article.description,
-      published_at: savedArticleTime,
-      link_url: article.url,
-      img_url: article.urlToImage,
-      source: article.source.name,
-    }),
-    headers: { 'Content-Type': 'application/json' },
-    credentials: 'include',
-  }).catch((err) => console.log(err));
+export const saveUserArticle = async (email, article, savedArticleTime) => {
+  try {
+    return fetch('/api/articles/save', {
+      method: 'POST',
+      body: JSON.stringify({
+        email,
+        author: article.author,
+        title: article.title,
+        description: article.description,
+        published_at: savedArticleTime,
+        link_url: article.url,
+        img_url: article.urlToImage,
+        source: article.source.name,
+      }),
+      headers: { 'Content-Type': 'application/json' },
+      credentials: 'include',
+    });
+  } catch (err) {
+    return console.log(err);
+  }
 };
 
 // delete article api call
-export const deleteSavedArticles = (email, articleLink) => {
-  return fetch('/api/articles/delete', {
-    method: 'DELETE',
-    body: JSON.stringify({
-      email: email,
-      link_url: articleLink,
-    }),
-    headers: { 'Content-Type': 'application/json' },
-    credentials: 'include',
-  }).catch((err) => console.log(err));
+export const deleteSavedArticles = async (email, articleLink) => {
+  try {
+    return fetch('/api/articles/delete', {
+      method: 'DELETE',
+      body: JSON.stringify({
+        email: email,
+        link_url: articleLink,
+      }),
+      headers: { 'Content-Type': 'application/json' },
+      credentials: 'include',
+    });
+  } catch (err) {
+    return console.log(err);
+  }
 };
